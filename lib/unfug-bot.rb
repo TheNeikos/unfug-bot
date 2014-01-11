e09473908dbffd5b32dad68a2bc762816c5612da
Matthias Beyer <mail@beyermatthias.de>
2014-01-11 20:54:08 +0100

    The config should return only available plugins if requested

--- lib/unfug-bot.rb
+++ lib/unfug-bot.rb
@@ -9,6 +9,16 @@ class Config < YAML
     self.define_method(sym, lambda { self[sym.to_s] } )
   end
 
+  def available_plugins
+    self.plugins.map do |pluginname|
+      begin
+        Object.get_const pluginname
+      rescue NameError
+        nil
+      end
+    end.compact
+  end
+
 end
 
 class BotRunner
@@ -22,23 +32,13 @@ class BotRunner
     end
   end
 
-  def strs_to_consts(strs)
-    strs.map do |s|
-      begin
-        Object.const_get s
-      rescue NameError
-        nil
-      end
-    end.compact
-  end
-
   def gen_bot_for_config c
     Cinch::Bot.new do
       configure do |conf|
         conf.server = c.server
         conf.channels = c.channels
         conf.nick = c.botnick
-        conf.plugins = strs_to_consts c.plugins
+        conf.plugins = c.available_plugins
         # AWESOME! ikr
       end
     end
